77f820f91f1da15a999b2534f5c6613078dafd9c
esemv07 <ruby.semaganda@gmail.com>
2026-04-11 11:36:30 +1000

    Add moveable train with app icons at locations

--- script.js
+++ script.js
@@ -1,3 +1,203 @@
+var mapTransform = "scale(2) translate(150px, -50px)";
+var leftText = "To File Forest";
+var rightText = "To Calculation Cove";
+var currentLocation = "Text Territory";
+var currentApp = document.querySelector(".notes")
+const locations = [
+    "Text Territory",
+    "Productivity Peaks",
+    "Calculation Cove",
+    "Web Waterhole",
+    "File Forest",
+]
+
+const backgroundMap = document.querySelector('.content');
+
+var welcomeScreen = document.querySelector("#welcome")
+
+var trainScreen = document.querySelector("#traincontainer")
+
+var mapLabels = document.querySelector('#map-labels')
+var leftButton = document.querySelector('#left-direction')
+var rightButton = document.querySelector('#right-direction')
+var directions = document.querySelector('#directions')
+var leftContent = document.querySelector("#left-text")
+var rightContent = document.querySelector("#right-text")
+var locationContent = document.querySelector(".location-label")
+
+
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+var content = document.querySelector('.content')
+
+welcomeScreenClose.addEventListener("click", function() {
+    closeWindow(welcomeScreen);
+    closeWindow(mapLabels);
+    mapOpen();
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+    mapClosed();
+    setTimeout(() => {
+        openWindow(welcomeScreen);
+        openWindow(mapLabels);
+    }, 2500);
+});
+
+
+function mapOpen() {
+    backgroundMap.style.setProperty('--map-transform', mapTransform);
+    setTimeout(() => {
+        openWindow(currentApp);
+        openWindow(trainScreen);
+    }, 2500)
+    leftButton.addEventListener("click", function() {
+        if (leftText == "To File Forest" && currentLocation != "File Forest") {
+            mapTransform = "scale(2) translate(100px, 100px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "File Forest";
+            leftText = "End of the Line";
+            rightText = "To Text Territory";
+            leftButton.classList.add("disabled");
+            currentApp = document.querySelector(".files");
+        }
+        if (leftText == "To Text Territory" && currentLocation != "Text Territory") {
+            mapTransform = "scale(2) translate(150px, -50px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "Text Territory";
+            leftText = "To File Forest";
+            rightText = "To Calculation Cove";
+            currentApp = document.querySelector(".notes");
+        }
+        if (leftText == "To Calculation Cove" && currentLocation != "Calculation Cove") {
+            mapTransform = "scale(2) translate(-300px, -100px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp);
+            }, 2500)
+            currentLocation = "Calculation Cove";
+            leftText = "To Text Territory";
+            rightText = "To Web Waterhole";
+            rightContent.style.fontSize = "16px"
+            currentApp = document.querySelector(".calc");
+        }
+        if (leftText == "To Web Waterhole" && currentLocation != "Web Waterhole") {
+            mapTransform = "scale(2) translate(-600px, -100px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "Web Waterhole";
+            leftText = "To Calculation Cove";
+            rightText = "To Productivity Peaks";
+            rightContent.style.fontSize = "12px"
+            rightButton.classList.remove("disabled");
+            currentApp = document.querySelector(".internet");
+        }
+        leftContent.textContent = leftText;
+        rightContent.textContent = rightText;
+        locationContent.textContent = currentLocation;
+    })
+    rightButton.addEventListener("click", function() {
+        if (rightText == "To Productivity Peaks" && currentLocation != "Productivity Peaks") {
+            mapTransform = "scale(2) translate(-550px, -250px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "Productivity Peaks";
+            leftText = "To Web Waterhole";
+            rightText = "End of the Line";
+            rightContent.style.fontSize = "16px"
+            rightButton.classList.add("disabled");
+            currentApp = document.querySelector(".todo");
+        }
+        if (rightText == "To Web Waterhole" && currentLocation != "Web Waterhole") {
+            mapTransform = "scale(2) translate(-600px, -100px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "Web Waterhole";
+            leftText = "To Calculation Cove";
+            rightText = "To Productivity Peaks";
+            rightContent.style.fontSize = "12px";
+            currentApp = document.querySelector(".internet");
+        }
+        if (rightText == "To Calculation Cove" && currentLocation != "Calculation Cove") {
+            mapTransform = "scale(2) translate(-300px, -100px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp);
+            }, 2500)
+            currentLocation = "Calculation Cove";
+            leftText = "To Text Territory";
+            rightText = "To Web Waterhole";
+            currentApp = document.querySelector(".calc");
+        }
+        if (rightText == "To Text Territory" && currentLocation != "Text Territory") {
+            mapTransform = "scale(2) translate(150px, -50px)"
+            backgroundMap.style.setProperty('--map-transform', mapTransform);
+            closeWindow(directions)
+            closeWindow(currentApp)
+            setTimeout(() => {
+                directions.style.display = "flex";
+                openWindow(currentApp)
+            }, 2500)
+            currentLocation = "Text Territory";
+            leftText = "To File Forest";
+            rightText = "To Calculation Cove";
+            leftButton.classList.remove("disabled");
+            currentApp = document.querySelector(".notes");
+        }
+        leftContent.textContent = leftText;
+        rightContent.textContent = rightText;
+        locationContent.textContent = currentLocation;
+    })
+}
+
+function mapClosed() {
+    closeWindow(trainScreen);
+    closeWindow(currentApp);
+    backgroundMap.style.setProperty('--map-transform', 'scale(1)');
+}
+
+
+function closeWindow(element) {
+    element.style.display = "none"
+}
+
+function openWindow(element) {
+    element.style.display = "block"
+}
+
 dragElement(document.getElementById("welcome"))
 
 function dragElement(element) {
@@ -39,5 +239,26 @@ function dragElement(element) {
     function stopDragging() {
         document.onmouseup = null;
         document.onmousemove = null;
+    }
+}
+
+var selectedIcon = undefined;
+
+function selectIcon(element) {
+    element.classList.add("selected");
+    selectedIcon = element
+}
+
+function deselectIcon(element) {
+    element.classList.remove("selected");
+    selectedIcon = undefined
+}
+
+function handleIconTap(element) {
+    if (element.classList.contains("selected")) {
+        deselectIcon(element)
+        openWindow(window)
+    } else {
+        selectIcon(element)
     }
 }
